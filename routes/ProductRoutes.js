dc5f0dd6f9605506ffa49474690a3814b753beb3
Rohit Badhai <rohitbadhai2001@gmail.com>
2023-12-17 14:23:11 +0530

    first commit

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -4,7 +4,6 @@ const ProductRouter=express.Router();
 ProductRouter.post("/products",createProduct)
 ProductRouter.get("/products",fetchAllProducts)
 ProductRouter.get("/products/:id",fetchProductById)
-
 ProductRouter.patch("/products/:id",updateProduct)
 
 
