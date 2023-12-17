47ac0e84e2f5ff0c26bd53de86eaa23b0f587576
Rohit Badhai <rohitbadhai2001@gmail.com>
2023-12-17 14:21:02 +0530

    .gitignore

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -4,6 +4,7 @@ const ProductRouter=express.Router();
 ProductRouter.post("/products",createProduct)
 ProductRouter.get("/products",fetchAllProducts)
 ProductRouter.get("/products/:id",fetchProductById)
+
 ProductRouter.patch("/products/:id",updateProduct)
 
 
